760484167daac6b54baa47c00d7a0adebe5f3e87
mihcristina <contato@michellicristina.com.br>
2022-05-16 10:33:54 -0300

    first commit

---
+++ Pensamentos/Controllers/QuotesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuotesViewController.swift
+//  Pensamentos
+//
+//  Created by Michelli Cristina de Paulo Lima on 16/05/22.
+//
+
+import UIKit
+
+class QuotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pensamentos/Controllers/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Pensamentos
+//
+//  Created by Michelli Cristina de Paulo Lima on 16/05/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
